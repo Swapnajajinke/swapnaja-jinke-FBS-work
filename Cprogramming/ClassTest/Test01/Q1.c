945a444c7f8f617201864a668a1c316bcad197f0
Swapnajajinke <jinkeswapnaja@gmail.com>
2025-06-05 12:39:17 +0530

    Add files via upload

---
+++ Cprogramming/ClassTest/Test01/Q1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int min,hr,sec,m;
+	printf("Enter hr,min,sec");
+	scanf("%d%d%d",&hr,&min,&sec);
+	int time=hr*3600+hr*60+30;
+	printf("the total second are %d");
+	
+}
---
+++ Cprogramming/ClassTest/Test01/test Q3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//int no=22340;
+	//int r1=no%d;
+	
+	
+	//printf("the last two digits are %d,r1)
+	
+	
+	int no;
+	printf("enter no");
+    scanf("%d",&no);
+    int r1=no%100;
+    printf("the last two digits are %d",r1);
+}
