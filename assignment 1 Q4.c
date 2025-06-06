3b3e51f219e9d00b44bcd4167dede7dcbd84e204
Swapnajajinke <jinkeswapnaja@gmail.com>
2025-06-06 12:06:26 +0530

    Add files via upload

---
+++ assignment 1 Q4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	char ch = 'y';
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+		printf("char is a is a vowel");
+	}
+	else{
+		printf("char is a consonent");
+	}
+}
---
+++ assignment 1 Q5 .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int age = 23; 
+    if (age >= 18)
+	{
+		printf("the person is eligible to vote");
+	}
+	else
+	if(age <= 18) 
+	{
+	printf("the person is not eligible to vote");
+}
+}
